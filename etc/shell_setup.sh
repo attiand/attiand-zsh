b2e0f99be7d1b6f4351cf566a5e8b91e7cbdd156
Mattias Andersson <mattias.j.andersson@gmail.com>
2025-04-23 21:03:27 +0200

    updated support for toolbx

--- etc/shell_setup.sh
+++ etc/shell_setup.sh
@@ -10,9 +10,14 @@ help() {
     "$@" --help 2>&1 | bathelp
 }
 
+toolbox_prompt_info
+
+
 if [ ! -f /run/.toolboxenv ]; then
-PROMPT='$(kube_ps1)'$PROMPT
+    PROMPT='$(kube_ps1)'$PROMPT
     export KUBE_PS1_SYMBOL_ENABLE=false
     export KUBE_PS1_PREFIX=[
     export KUBE_PS1_SUFFIX=]
+else
+    PROMPT="%{$fg_bold[blue]%}[%{$reset_color%}$(toolbox_prompt_name)%{$fg_bold[blue]%}]%{$reset_color%}$PROMPT"
 fi
